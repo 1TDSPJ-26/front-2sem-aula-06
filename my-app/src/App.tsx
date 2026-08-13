d2cbae5483f652d59dc3eba1029b5f831b7fb503
SophiaS4nts <sophia.sophis@icloud.com>
2026-08-13 12:04:14 -0300

    feat: meu primeiro componente

---
+++ my-app/src/App.tsx
@@ -0,0 +1,18 @@
+
+//Aprender a construir um componete funcional 
+
+
+export default function App(){
+  //Area reservada para declaracões
+
+  const nome = "Ale";
+
+
+  return(
+    <div>
+      {/*O HTML aqui é na verdade XML!*/}
+      <h1>Meu componente</h1>
+      <h2>Autor; {nome}</h2>
+    </div>
+  );
+}
