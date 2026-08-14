943ca383e3866dc1109b94c8e170b8dd7d87cdc7
BeaUrbano <beatrizurbanosilva@gmail.com>
2026-08-14 12:15:13 -0300

    feat: colocando primeiros componentes

---
+++ my-app/src/App.tsx
@@ -0,0 +1,25 @@
+export default function App() {
+
+  return (
+    <div>
+      <header>
+        <h1>Meus Componentes</h1>
+        <nav>
+          <ul>
+            <li>Item-1</li>
+            <li>Item-2</li>
+            <li>Item-3</li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <h2>Conteúdo</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia aliquid porro cum illo eveniet, quod cumque nesciunt ipsa, reprehenderit hic fugiat. Facere aliquid veritatis modi doloremque labore possimus eveniet recusandae, tempora eos at incidunt, rerum nulla totam asperiores explicabo magni quam adipisci esse earum, quisquam mollitia! Omnis libero repellat consequatur.</p>
+      </main>
+      <footer>
+        <p>Todos os direitos reservados. &copy;</p>
+      </footer>
+    </div>
+  );
+}
+
