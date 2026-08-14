dd5f3f0130ff615b1a2b86dc60f465a42f3d952f
Renatoruiz1 <ruizrenato511@gmail.com>
2026-08-14 12:25:56 -0300

    feat: finalizando as atualizações que eu esqueci

---
+++ my-app/src/App.tsx
@@ -0,0 +1,23 @@
+export default function App(){
+
+  return(
+    <div>
+      <header>
+        <h1>Meus Componentes</h1>
+      <nav>
+        <ul>
+          <li>Item-1</li>
+          <li>Item-2</li>
+          <li>Item-3</li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <h2>Conteudo</h2>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt veniam, id obcaecati at blanditiis corporis voluptatibus cumque commodi quos laboriosam sed fugit voluptatem vitae excepturi quod perspiciatis et a, ipsam odit asperiores esse harum nihil. Exercitationem voluptatum sunt ipsam sint, veniam, quisquam consequuntur id iusto distinctio vel est, nisi ea consequatur sed impedit. Facere, laborum nostrum architecto obcaecati eligendi a!</p>
+    </main>
+    <footer>Tods os direitos reservados Christian</footer>
+    </div>
+
+  );
+}
