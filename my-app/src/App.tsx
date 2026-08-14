24a5bd6963d3de62b61b90fb7404251a09634d55
alexsandrompr <mandupiresa@gmail.com>
2026-08-14 12:15:37 -0300

    feat: criação do alex.tsx e alterações

---
+++ my-app/src/App.tsx
@@ -0,0 +1,26 @@
+import Alex from "./components/Nome/Alex"
+
+export default function App(){
+    return(
+        <div>
+            <header>
+                <h1>Meus Componentes</h1>
+                <nav>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </nav>
+            </header>
+            <main>
+                <h2>Contéudo</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias doloremque atque numquam, delectus illo necessitatibus earum aliquam, voluptatum natus placeat ipsam eum impedit ipsum. Dolores animi enim quas vitae aut!</p>
+            </main>
+           <Alex/>
+                       <footer>
+                <p>Todos os direitos reservados. &copy;</p>
+            </footer>
+        </div>
+    )
+}
