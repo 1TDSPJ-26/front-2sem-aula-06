b05889f844fc993260c8300fb5b2f6c7e80dfca5
joaovjbernardo <jjvictorbernardo@gmail.com>
2026-08-14 12:12:52 -0300

    feat: Publicando o primeiro componente

---
+++ my-app/src/App.tsx
@@ -0,0 +1,24 @@
+import Joaovictordjesus from "./components/Nome/Joaovictordjesus"
+
+export default function App() {
+  return (
+    <div>
+      <header>
+        <h1>Meus Componentes</h1>
+        <nav>
+          <ul>
+            <li>Item-1</li>
+            <li>Item-1</li>
+            <li>Item-1</li>
+            <li>Item-1</li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Joaovictordjesus/>
+        <h2>Conteudo</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestias, quasi eveniet quis ipsum ipsam optio sed, odit placeat beatae qui delectus dicta excepturi repellendus nam at architecto impedit nisi.</p>
+      </main>
+    </div>
+  );
+}
