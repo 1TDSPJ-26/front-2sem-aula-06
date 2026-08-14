d8e6e3d6f0024291c8a482ac8cc1bf054055603a
LeonardoSilva1203 <leonardosilva120308@gmail.com>
2026-08-14 12:04:39 -0300

    feat: Publicando primeiro componente integrado

---
+++ my-app/src/App.tsx
@@ -0,0 +1,27 @@
+import Leleo from "./components/Nome/Leleo"
+
+export default function App(){
+
+    return(
+        <div>
+          <header>
+            <h1>Meus Componentes</h1>
+            <nav>
+            <ul>
+            <li>Item-1</li>
+            <li>Item-2</li>
+            <li>Item-3</li>
+            </ul>
+            </nav>
+          </header>
+          <main>
+            <Leleo/>
+          <h2>Conteúdo</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, repellendus rerum minus maiores in fugiat architecto voluptatum aliquam! Accusantium officia amet eligendi ex quibusdam repellendus porro, vero facilis nostrum illum corporis dolore iure nam, id asperiores molestiae numquam consectetur harum magni, quidem tenetur ducimus odit in. Enim tempora quam saepe soluta, nostrum placeat culpa ut ex ratione quaerat non quo molestiae sit provident dolor maiores necessitatibus sapiente debitis itaque repudiandae delectus vitae! Omnis, natus quasi sunt nihil voluptatibus dolorum optio reiciendis maiores sit tempora deleniti ad neque sint provident similique unde, hic autem itaque, illum cumque labore in quisquam non. Harum et quas ducimus commodi numquam nemo vel debitis possimus! Tempore officia consequuntur velit. Earum, hic mollitia, aut officia animi repellat, enim libero voluptas quo porro cum? Officia molestiae possimus voluptate deleniti, adipisci hic animi tenetur ea veniam minima mollitia sunt enim nam repudiandae aspernatur corrupti ex fugiat consequatur. Porro.</p>
+          </main>
+          <footer>
+          <p>Todos os direitos reservados. &copy</p>
+          </footer>
+        </div>
+    );
+}
---
+++ my-app/src/components/Nome/Leleo.tsx
@@ -0,0 +1,7 @@
+export default function Leleo(){
+    return(
+        <div>
+            <h2>Leonardo Arnaldo Cerqueira Da Silva</h2>
+        </div>
+    );
+}
