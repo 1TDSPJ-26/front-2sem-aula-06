6e8a69ac68eb4b5dede6812c4abbe5ee605b3da6
henriqueosuka <henriqueosuka@gmail.com>
2026-08-14 11:44:32 -0300

    feat: limpeza local do boiler plate

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,10 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-<<<<<<< HEAD
-import './index.css'
-=======
 
->>>>>>> develop
+
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
