366c9dab16e3a05cb1ef3302cadde273f9e1aa17
flavio <rm571211@fiap.com.br>
2026-08-14 12:04:34 -0300

    feat: publicando primeiro componente integrado

---
+++ my-app/src/App.tsx
@@ -0,0 +1,38 @@
+import Flavio from "./components/nome/Flavio"
+
+export default function App() {
+
+
+
+
+
+
+
+
+
+  return(
+    
+    <div>
+      <header>
+        <h1>meus componentes</h1>
+        <nav>
+          <ul>
+            <li>item1</li>
+            <li>item2</li>
+            <li>item3</li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Flavio/>
+        <h2>conteudo</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis quo quod est aliquid earum, laudantium perspiciatis accusantium doloremque provident iusto quidem porro ullam, illo sint dolores vel repellat neque libero.</p>
+      </main>
+      <footer>
+        <p>todos os direitos reservados. &copy;</p>
+      </footer>
+    </div>
+  );
+
+
+}
