801a385cc47cadf245851875f8a19696a970234a
SophiaS4nts <sophia.sophis@icloud.com>
2026-08-14 11:54:16 -0300

    feat: limpeza total do boinel plate

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,6 +1,19 @@
+<!doctype html>
+  <html lang="pt-br">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Meus Componentes</title>
+    </head>
+    <body>
+      <div id="root"></div>
+      <script type="module" src="/src/main.tsx"></script>
+    </body>
+  </html>
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
@@ -8,3 +21,6 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 )
+
+
+ 
