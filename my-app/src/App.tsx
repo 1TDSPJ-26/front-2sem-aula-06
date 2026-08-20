ac70fb4af8733b0b34c10d48a221e41896ee9630
matheusfa08 <littlepacoquinha@gmail.com>
2026-08-20 12:18:04 -0300

    feat: Limpando boiler plate e iniciando xml, jsx, tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,47 +1,15 @@
+import Cabecalho from "./components/cabecalho/cabecalho"
 
-import SophiaSil from "./components/Nome/SophiaSil"
-import Henrique from "./components/Nome/Henrique"
-import Leleo from "./components/Nome/Leleo"
-import MatheusFerreira from "./components/Nome/MatheusFerreira"
-import Renatoruiz from "./components/Nome/Renatoruiz"
-import Joaovictordjesus from "./components/Nome/Joaovictordjesus"
-import Bea from "./components/Nome/Bea"
-import Breno from "./components/Nome/Breno"
-import ChristianVerzellesi from "./components/Nome/ChristianVerzellesi"
-import Alex from "./components/Nome/Alex"
-
-export default function App() {
-
-  return (
-    <div>
-      <header>
-        <h1>Meus Componentes</h1>
-        <nav>
-          <ul>
-            <li>Item-1</li>
-            <li>Item-2</li>
-            <li>Item-3</li>
-          </ul>
-        </nav>
-      </header>
-      <main>
-        <h2>Conteudo</h2>
-        <SophiaSil />
-        <Leleo />
-        <MatheusFerreira />
-        <Renatoruiz />
-        <Joaovictordjesus />
-        <Bea />
-        <Breno/>
-        <ChristianVerzellesi/>
-        <Henrique/>
-        <Alex/>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt veniam, id obcaecati at blanditiis corporis voluptatibus cumque commodi quos laboriosam sed fugit voluptatem vitae excepturi quod perspiciatis et a, ipsam odit asperiores esse harum nihil. Exercitationem voluptatum sunt ipsam sint, veniam, quisquam consequuntur id iusto distinctio vel est, nisi ea consequatur sed impedit. Facere, laborum nostrum architecto obcaecati eligendi a!</p>
-      </main>
-      <footer>Tods os direitos reservados Christian</footer>
-    </div>
-
-  );
+export default function App(){
+    return(
+            <div>
+                <Cabecalho></Cabecalho>
+                <main>
+                    <h2>Conteúdo</h2>
+                </main>
+                <footer>
+                    <p>&copy;2026 - Todos os direitos reservados</p>
+                </footer>
+            </div>
+    )
 }
-
-
