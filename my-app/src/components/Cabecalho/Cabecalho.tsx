71f356a0a43456c5649daf1ad9c85e16e9558ee6
milasnt <camilassntt@gmail.com>
2026-08-21 09:29:01 -0300

    feat: iniciando meu boiler plate com modularização

---
+++ my-app/src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,10 @@
+export default function Cabecalho() {
+    return (
+        <div>
+            <header>
+                <h1>Meu boiler-plate</h1>
+            </header>
+
+        </div>
+            );
+}
---
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -0,0 +1,7 @@
+export default function Conteudo(){
+    return(
+        <main>
+            <h2>Conteúdo.</h2>
+        </main>
+    );
+}
