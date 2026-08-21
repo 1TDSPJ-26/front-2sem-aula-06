15c7f9ae2ff9bb1485a5ebdfc2bd030022bb7e97
TailyiDev <tailyni80@gmail.com>
2026-08-21 08:03:10 -0300

    feat: cria componentes Conteudo e Rodape

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,24 +1,21 @@
 import Cabecalho from "./components/Cabecalho/Cabecalho";
+import Conteudo from "./components/Conteudo/Conteudo";
+import Rodape from "./components/Rodape/Rodape";
 
-export default function App(){
+export default function App() {
 
-  return(
+  return (
     <div>
-      
-      <Cabecalho/>
 
-    {/* Para a próxima aula crie os componentes Conteudo e Rodape e importem 
+      <Cabecalho />
+      {/* Para a próxima aula crie os componentes Conteudo e Rodape e importem 
+
       automáticamente nos lugares dos JSX aqui em App. */}
-
-      <main>
-          <h2>Conteúdo</h2>
-      </main>
-
-      <footer>
-          <p>&copy;2026 - Todos os direitos reservados</p>
-      </footer>
       
+      <Conteudo />
+
+      <Rodape />
+
     </div>
   );
 }
-
