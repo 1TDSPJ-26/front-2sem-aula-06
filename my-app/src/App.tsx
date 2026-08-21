7eaa6ff4b1c311b7423789224fb9bba693b0c171
TailyiDev <tailyni80@gmail.com>
2026-08-21 07:45:36 -0300

    feat : Iniciando meu boiler plate com modularização.

---
+++ my-app/src/App.tsx
@@ -0,0 +1,24 @@
+import Cabecalho from "./components/Cabecalho/Cabecalho";
+
+export default function App(){
+
+  return(
+    <div>
+      
+      <Cabecalho/>
+
+    {/* Para a próxima aula crie os componentes Conteudo e Rodape e importem 
+      automáticamente nos lugares dos JSX aqui em App. */}
+
+      <main>
+          <h2>Conteúdo</h2>
+      </main>
+
+      <footer>
+          <p>&copy;2026 - Todos os direitos reservados</p>
+      </footer>
+      
+    </div>
+  );
+}
+
