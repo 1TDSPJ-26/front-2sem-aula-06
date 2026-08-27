a555014bbbc104f78ef1dcc67f35585b5154252c
milasnt <camilaassnt@gmail.com>
2026-08-27 12:06:28 -0300

    feat: criação de rotas com react-router e Outlet

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,12 @@
+import { Outlet } from "react-router"
 import Cabecalho from "./components/Cabecalho/Cabecalho"
-import Conteudo from "./components/Conteudo/Conteudo"
 import Footer from "./components/Footer/Footer"
 
 export default function App() {
   return (
     <div>
       <Cabecalho />
-      <Conteudo />
+      <Outlet />
       <Footer /> 
     </div>
   )
---
+++ my-app/src/routes/Error/index.tsx
@@ -0,0 +1,9 @@
+export default function Error() {
+    return (
+        <div>
+            <main>
+                <h1>404 - Página não encontrada</h1>
+            </main>
+        </div>
+    );
+}
