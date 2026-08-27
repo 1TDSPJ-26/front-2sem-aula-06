7fe9e4620782af85875545678c9b24afa46c97bb
joaovjbernardo <jjvictorbernardo@gmail.com>
2026-08-27 12:07:17 -0300

    feat: Criacap de rotas com react

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -3,6 +3,21 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import "./globas.css"
 
+
+//importal os objetos de rota
+import { createBrowserRouter, RouterProvider} from 'react-router'
+
+//impotar os componentes de rotas:
+import Home from './routes/home/index.tsx';
+import Produtos from './routes/Produtos/index.tsx';
+import EditarProdutos from './routes/EditarProdutos/index.tsx';
+import Error from './routes/Error/index.tsx';
+
+/Criando a objeto de rotas:
+constroute = createBrowserRouter([
+  {path "/", element: </app>, errorElementById('root')!}
+])
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
---
+++ my-app/src/routes/Error/index.tsx
@@ -0,0 +1,7 @@
+export default function Error(){
+    export(
+        <main>
+            <h2>404 - Pagina não encontrada</h2>
+        </main>
+    )
+}
