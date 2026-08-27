21e3c2e440b6b351580729de4402f2fa7ad50275
SophiaS4nts <sophia.sophis@icloud.com>
2026-08-27 11:40:17 -0300

    feat: Pacotes foram adicionados e testados- react-router, não testado, tailwindcss testado e aprovados

--- my-app/src/components/cabecalho/cabecalho.tsx
+++ my-app/src/components/cabecalho/cabecalho.tsx
@@ -1,7 +1,7 @@
 export default function Cabecalho(){
     return(
-        <header>
-          <h1>Meu boiler</h1>
+        <header className="bg-lime-100 text-gray-800 text-center">
+          <h1 className="minhaClasse">Meu boiler</h1>
       </header>
     );
 }
