2c17ecef94f909e64f4299b995dca216912c2d57
sophRmd <sophia.ramada02@gmail.com>
2026-08-27 12:05:34 -0300

    feat: Criação de rotas com react-router e Outlet

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,9 +1,11 @@
+import { Outlet } from "react-router/internal/react-server-client"
 import Cabecalho from "./components/cabecalho/Cabecalho"
 
 export default function App() {
   return(
     <div>
       <Cabecalho/>
+      <Outlet/>
       <main>
         <h2>Conteúdo</h2>
       </main>
