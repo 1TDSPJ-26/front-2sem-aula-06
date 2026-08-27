6201bb15b0ca675a524c923790e13a1ca7bff135
matheusfa08 <littlepacoquinha@gmail.com>
2026-08-27 11:41:09 -0300

    feat: instalado tailwind, e pacotes atualizados para ele

--- my-app/src/components/cabecalho/Cabecalho.tsx
+++ my-app/src/components/cabecalho/Cabecalho.tsx
@@ -1,6 +1,6 @@
 export default function Cabecalho(){
     return(
-        <header>
+        <header className = "bg-line-100 text-dark-500 text-center align-middle">
             <h1>Meu Boiler Plate</h1>
         </header>
     )
