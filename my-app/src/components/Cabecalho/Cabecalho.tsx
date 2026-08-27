f5cf3a83dd25b2ae724bf45e685cb909d91b021f
brenoell <brenobell03@gmail.com>
2026-08-27 11:42:29 -0300

    feat: mexendo com tailwindcss

--- my-app/src/components/Cabecalho/Cabecalho.tsx
+++ my-app/src/components/Cabecalho/Cabecalho.tsx
@@ -1,6 +1,6 @@
 export default function Cabecalho() {
   return (
-    <header>
+    <header className="bg-lime-500 text-amber-600 align-text-center">
       <h1>Meu boiler plate</h1>
     </header>
   );
