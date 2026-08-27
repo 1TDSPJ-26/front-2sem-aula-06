c4c67d44645404de9c2685e2834ffd5d4602052f
brenoell <brenobell03@gmail.com>
2026-08-27 12:12:32 -0300

    feat: finalizando rotas

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,17 +1,8 @@
+import Cabecalho from "./components/Cabecalho/Cabecalho";
+
 export default function App(){
 
   return(
-    <body>
-      
-      <header>
-          <h1>Meu boiler plate</h1>
-      </header>
-      <main>
-          <h2>Conteúdo</h2>
-      </main>
-      <footer>
-          <p>@copy;2026 Todos os direitos reservados</p>
-      </footer>
-    </body>
+    <Cabecalho/>
   );
 }
---
+++ my-app/src/routes/Error/index.tsx
@@ -0,0 +1,7 @@
+export default function Error(){
+    return(
+        <main>
+            <h2>Home</h2>
+        </main>
+    )
+}
