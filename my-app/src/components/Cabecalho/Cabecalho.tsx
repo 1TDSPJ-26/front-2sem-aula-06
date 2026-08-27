0be86ea087047c949e511008d4c957a7dd00f56d
joaovjbernardo <jjvictorbernardo@gmail.com>
2026-08-27 11:40:21 -0300

    feat: Pacotes foram adicionados e testados

--- my-app/src/components/Cabecalho/Cabecalho.tsx
+++ my-app/src/components/Cabecalho/Cabecalho.tsx
@@ -1,7 +1,7 @@
 export default function Cabecalho() {
   return (
-    <header>
-      <h1>Meu boiler plate</h1>
+    <header className=" bg-line-100 text-gray-800 text-center">
+      <h1 className="minha classe">Meu boiler plate</h1>
     </header>
   );
 }
